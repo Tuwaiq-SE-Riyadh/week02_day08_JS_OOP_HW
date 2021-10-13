ea3a3e9ad8a84c87086b1493f7c6a730a3b389ef
noura <norah@MacBook-Air-norah.local>
2021-10-13 09:10:39 +0300

    my hw

---
+++ main.js
@@ -0,0 +1,22 @@
+const myfoftir={
+    title:"Puff the Magic Dragon lasts",
+    duration:30,
+    stars:["Puff","Jackie","Living Sneezes"],
+ 
+    greeting() {
+        return this.title + " for "+this.duration+ "  minutes Stars:"+ this.stars
+      }
+}
+
+console.log(myfoftir.greeting())
+// myfoftir.array.forEach(element => {
+//     onsole.log( console.log(myfoftir.title +  myfoftir.duration+  myfoftir.stars) );
+// });   
+   
+   
+
+//     console.log( console.log(myfoftir.title +  myfoftir.duration+  myfoftir.stars) );
+// });
+
+
+//   
